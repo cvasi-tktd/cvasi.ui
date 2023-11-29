671540acf12de295e83a5510eeecd27c3d91dd14
Torben Wittwer <t.wittwer@rifcon.de>
2023-11-29 15:07:10 +0100

    added parameter description table as raw data and rda

---
+++ data-raw/param_descr.R
@@ -0,0 +1,6 @@
+## code to prepare `param_descr` dataset goes here
+filepath <- paste0(here::here(), "/inst/extdata/", "parameter_descriptions.csv")
+parameter_descriptions <- read.table(filepath, sep = ";", header = TRUE)
+
+
+usethis::use_data(parameter_descriptions, overwrite = TRUE)
--- dev/02_dev.R
+++ dev/02_dev.R
@@ -59,7 +59,7 @@ golem::add_css_file("check_input")
 
 ## Add internal datasets ----
 ## If you have data in your package
-#usethis::use_data_raw(name = "my_dataset", open = FALSE)
+usethis::use_data_raw(name = "param_descr", open = TRUE)
 
 ## Tests ----
 ## Add one line by test you want to create
