0c74b873ade0e45973bccec7462691cf66501366
Torben Wittwer <t.wittwer@rifcon.de>
2023-12-14 15:42:43 +0100

    Add EPx in a moving time window and plot to predictions

--- R/mod_prediction.R
+++ R/mod_prediction.R
@@ -17,9 +17,12 @@ mod_prediction_ui <- function(id){
     div(
       plotOutput(ns("stat_var_plot"), width = "100%", height = "600px", fill = FALSE), 
       style = "max-width: 800px;"
-      )
-    
-
+      ),
+    tags$hr(),
+    div(
+      plotOutput(ns("epx_mtw_plot"), width = "100%", height = "600px", fill = FALSE), 
+      style = "max-width: 800px;"
+    )
   )  
   
 }
@@ -52,17 +55,18 @@ mod_prediction_server <- function(id, modeldat, exposure_time_series, forcings_t
       get_required(modeldat(), "forcings")
     )
     
-    ## Predict button observer ---------------------------
+    # Predict button observer -------------------------------------------------
     observeEvent(input[["predict"]],{
 
       shinybusy::show_modal_spinner(text = "Predicting...")
+      
+      ## Simulate --------------------------------------------------------------
       tryCatch({
         shinyjs::html("error_text_sv", "")
         sim_result[["stat_var"]] <- NULL
 
         model_input <- modeldat()
         
-
         if (length(req_forcings()) > 0){
           model_input <- model_input %>% 
                      set_forcings(forcings_time_series()
@@ -74,16 +78,41 @@ mod_prediction_server <- function(id, modeldat, exposure_time_series, forcings_t
                                                    param_sample = NULL)
           
         },
-
         warning = function(cond) warnings_f(cond, id = "error_text_sv"),
-
         error = function(cond) error_f(cond, id = "error_text_sv")
       )
+      
+      # Calculate EPx ----------------------------------------------------------
+      shinybusy::update_modal_spinner(text = "Calculating EPx in moving time windows...")
 
+      
+      tryCatch({
+        shinyjs::html("error_text_sv", "")
+        sim_result[["epx_mtw"]] <- NULL
+        #browser()  
+        model_input <- modeldat() %>% 
+          set_exposure(exposure_time_series() %>% dplyr::select(time,conc))
+        
+        if (length(req_forcings()) > 0){
+          model_input <- model_input %>% 
+            set_forcings(forcings_time_series()
+            )
+        }
+
+        sim_result[["epx_mtw"]] <- model_input %>% 
+          epx_mtw(level = 10, factor_cutoff = 1000,
+                  window_length = 7, window_interval = 1)
+        
+      },
+      warning = function(cond) warnings_f(cond, id = "error_text_sv"),
+      error = function(cond) error_f(cond, id = "error_text_sv")
+      )
+      
       shinybusy::remove_modal_spinner()
 
     }, ignoreInit = TRUE)
     
+    # Warning/Error output functions -------------------------------------------
     warnings_f <- function(cond, id){
       shinyjs::html(id = id,
                     html = paste0("<div class = \"text-warning\">",as.character(cond),"</div>"),
@@ -96,7 +125,8 @@ mod_prediction_server <- function(id, modeldat, exposure_time_series, forcings_t
     }
 
     
-    
+    # Render plots -------------------------------------------------------------
+    ## Simulation results ------------------------------------------------------
     output[["stat_var_plot"]] <- renderPlot({
       req(length(sim_result[["stat_var"]]) > 0)
       
@@ -118,6 +148,15 @@ mod_prediction_server <- function(id, modeldat, exposure_time_series, forcings_t
         #ggplot2::theme(legend.text=ggplot2::element_text(size=12))
 
     })
+    
+    ## EPx results -------------------------------------------------------------
+    output[["epx_mtw_plot"]] <- renderPlot({
+      req(length(sim_result[["epx_mtw"]]) > 0)
+      exposure <- isolate(exposure_time_series()[,c("time","conc")])
+      plot_EPx(EPx_ts = sim_result[["epx_mtw"]],
+               exposure_ts = exposure)
+    })
+    
 
   })
 }
