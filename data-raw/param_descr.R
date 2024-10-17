627093e39044fc2e1084a1b82ffa4e20b49436bd
Nils Kehrein <nils.kehrein@gmail.com>
2024-10-17 13:06:32 +0200

    fix, enforce data type of 'parameter_descriptions' dataset

--- data-raw/param_descr.R
+++ data-raw/param_descr.R
@@ -2,7 +2,8 @@
 filepath <- file.path("data-raw", "parameter_descriptions.xlsx")
 parameter_descriptions <- readxl::read_xlsx(filepath) %>%
   dplyr::mutate(lower.boundary=as.numeric(lower.boundary),
-                upper.boundary=as.numeric(upper.boundary))
+                upper.boundary=as.numeric(upper.boundary)) %>%
+  as.data.frame()
 
 usethis::use_data(parameter_descriptions, overwrite = TRUE)
 rm(filepath, parameter_descriptions)
